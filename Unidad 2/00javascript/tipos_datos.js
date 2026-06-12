8b7feea2071feb4b10d378218b0fb56697b9c132
Frozt98 <andres10sala98@gmail.com>
2026-06-12 13:07:03 -0500

    Feat: Learning javascript for noobcisc

---
+++ Unidad 2/00javascript/tipos_datos.js
@@ -0,0 +1,36 @@
+// PRIMITIVOS
+let nombre = 'Maria'
+
+//numerico
+let edad = 25
+
+//booleano
+let esMayordeEdad = true
+
+//null
+let noHayvalor = null
+
+// undefined
+let noDefinido = undefined
+
+//symbol valor unico
+let simbolunico = Sumbol('unico')
+
+// datos gigantes hight
+let numeroGrande = 2n
+
+//COMPLETOS
+// object
+let carro ={
+    marca : 'Tesla',
+    modelo: 'Model s'
+}
+
+//arrays
+let frutas = ['manzana','pera','piña']
+
+//funciones
+function saludar(){}
+
+
+
