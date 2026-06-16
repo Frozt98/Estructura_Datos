2f755e28477ab1e3edd303c08aa14a1b9dee63b3
Frozt98 <andres10sala98@gmail.com>
2026-06-16 11:36:27 -0500

    Feat: Clase de la fecha 16/06/2026, Jose Salazar

---
+++ Unidad 2/00javascript/class-conversion.js
@@ -0,0 +1,14 @@
+const string = '47'
+const integer = parseInt(string)
+console.log(typeof integer)
+
+const stringdecimal = '0.123'
+const decimal = parseFloat(stringdecimal)
+console.log(typeof decimal)
+
+const binary = '10010'
+const decimalBinary = parseInt(binary, 2)
+console.log(decimalBinary)
+
+const suma = '5' + 3
+console.log(suma)
---
+++ Unidad 2/00javascript/class-number.js
@@ -0,0 +1,35 @@
+//Tipos  de datos numericos
+//enteros y decimal
+
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal) 
+
+//Notacion cientifica
+const cientifico = 5e3
+
+// Representacion de otros contextos
+const infinito = Infinity
+const noEsunNumero = NaN
+
+//Operaciones matematicas
+//Sumas y Restas
+const suma = 3 + 4
+const resta = 8 - 5
+const multiplicacion = 7 * 4
+const division = 8/6
+
+//Modulos de exponenciacion
+const modulo= 15 % 8
+//Resultado de presicion
+const exponenciacion = 0.1 * 0.2
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(3))
+
+const raizcuadrada = Math.sqrt(89)
+const valorAbosulto = Math.abs(-75)
+const aleatorio = Math.random()
+console.log(raizcuadrada)
+console.log(valorAbosulto)
+console.log(aleatorio)
+
